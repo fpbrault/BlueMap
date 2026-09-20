16f6b1e3908305700f9218732ebd416a1c804ef4
Felix Perron-Brault <fpbrault@gmail.com>
2026-09-20 13:39:11 -0400

    build: configure BlueMapModelLoaders 0.5.0 Java 21 addon

---
+++ compat/model-loaders-addon/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    bluemap.java
+    alias(libs.plugins.shadow)
+}
+
+group = "me.owies"
+version = "0.5.0-java21"
+
+dependencies {
+    implementation("com.technicjelle:BMUtils:4.3.1")
+    implementation(libs.caffeine)
+
+    compileOnly(project(":common"))
+    compileOnly(project(":core"))
+    compileOnly("de.bluecolored:bluemap-api")
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+}
+
+tasks.shadowJar {
+    relocate("com.technicjelle.BMUtils", "${project.group}.${project.name}.BMUtils")
+    relocate("com.github.benmanes.caffeine", "${project.group}.${project.name}.caffeine")
+    relocate("org.checkerframework", "${project.group}.${project.name}.checkerframework")
+    relocate("com.google.errorprone", "${project.group}.${project.name}.errorprone")
+    relocate("org.intellij", "${project.group}.${project.name}.intellij")
+    relocate("org.jetbrains", "${project.group}.${project.name}.jetbrains")
+    archiveFileName = "BlueMapModelLoaders-${project.version}.jar"
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.processResources {
+    filesMatching("bluemap.addon.json") {
+        expand("version" to project.version)
+    }
+}
